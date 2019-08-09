6d38eec9e3954a05ac6909637a9ddf3f5f3ec797
Chris Patrick <atomize101@gmail.com>
2019-08-09 17:57:44 -0400

    Adding in square colors

--- colorGuess.js
+++ colorGuess.js
@@ -2,15 +2,33 @@
 
 var colors = ['rgb(255, 0, 0)',
                 'rgb(255, 255, 0)',
-                'rgb(255, 0, 0)',
                 'rgb(0, 255, 0)',
                 'rgb(0, 255, 255)',
+                'rgb(0, 0, 255)',
                 'rgb(255, 0, 255)'
 ];
 
-var boxes = document.querySelectorAll('div');
+var pickedColor = colors[3];
+
+var boxes = document.querySelectorAll('.square');
+var colorDisplay = document.getElementById('colorDisplay');
+
+colorDisplay.textContent = pickedColor;
+
 
 for(var i=0; i < boxes.length; i++) {
-    console.log(boxes[i]);
     boxes[i].style.backgroundColor = colors[i];
+    
+    boxes[i].addEventListener('click', () => {
+        console.log(this.style.backgroundColor);
+        var clickedColor = this.style.backgroundColor;
+
+        if(clickedColor === pickedColor) {
+            alert('Correct');
+        }
+        else {
+            alert('Wrong color!');
+        }
+    });
 }
+
