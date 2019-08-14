b554bb4108ed053996c960a995a79fd574217db4
Chris Patrick <atomize101@gmail.com>
2019-08-14 14:08:41 -0400

    Adding in random color functionality.

--- colorGuess.js
+++ colorGuess.js
@@ -1,17 +1,12 @@
 
 
-var colors = ['rgb(255, 0, 0)',
-                'rgb(255, 255, 0)',
-                'rgb(0, 255, 0)',
-                'rgb(0, 255, 255)',
-                'rgb(0, 0, 255)',
-                'rgb(255, 0, 255)'
-];
+var colors = generateRandomColors(6);
 
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 
 var boxes = document.querySelectorAll('.square');
 var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.getElementById('message');
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,10 +18,40 @@ for(var i=0; i < boxes.length; i++) {
         var clickedColor = this.style.backgroundColor;
 
         if(clickedColor === pickedColor) {
-            alert('Correct');
+            messageDisplay.textContent = "Correct!";
+            changeColors(pickedColor);
         }
         else {
-            alert('Wrong color!');
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try again";
         }
     });
+}
+
+function changeColors(color) {
+    for(var i = 0; i < boxes.length; i++) {
+        boxes[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function generateRandomColors(num) {
+    var arr = [];
+    for(var i = 0; i < num; i++) {
+        //loop through and generate a random color to add to the array.
+        arr.push(randomColor());
+    }
+
+    return arr;
+}
+
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + "," + g + "," + b + ")";
 }
